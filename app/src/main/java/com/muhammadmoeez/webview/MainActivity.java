6f84e9a3bf390c019d60d2f7396380e86eef1e5b
Muhammadmoeez <mohammadmoeez417@gmail.com>
2022-06-29 17:43:48 +0500

    first commit

---
+++ app/src/main/java/com/muhammadmoeez/webview/MainActivity.java
@@ -0,0 +1,38 @@
+package com.muhammadmoeez.webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.msoltech.com/");
+        webView.setWebViewClient(new WebViewClient());
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (webView.canGoBack()){
+            webView.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
+
+    }
+}
